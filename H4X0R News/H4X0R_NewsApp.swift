0f9908b177d2703c69616e232745be2296bb0b8e
Zsolt Kébel <kebel.zsolt@gmail.com>
2021-10-09 22:35:51 +0100

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Zsolt Kébel on 09/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
